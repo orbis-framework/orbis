e5adcfd9fdf798fc55e484cb674673e5361f3a8d
im-adhavan <adhavanmurugaiyan@gmail.com>
2026-02-08 18:45:31 +0530

    tensor: add Tensor-Tensor ops with correct autograd support

--- orbis/tensor/tensor.py
+++ orbis/tensor/tensor.py
@@ -6,8 +6,15 @@ class Tensor:
         self._backward = lambda: None
         self._parents = []
 
+    def __repr__(self):
+        return f"Tensor(value={self.value}, grad={self.grad})"
+
     def backward(self):
+        """
+        Backpropagate gradients from this tensor.
+        """
         self.grad = 1.0
+
         topo = []
         visited = set()
 
@@ -19,5 +26,55 @@ class Tensor:
                 topo.append(t)
 
         build(self)
+
         for t in reversed(topo):
             t._backward()
+
+    def __add__(self, other):
+        if not isinstance(other, Tensor):
+            other = Tensor(other)
+
+        out = Tensor(self.value + other.value)
+        out._parents = [self, other]
+
+        def _backward():
+            self.grad += out.grad
+            other.grad += out.grad
+
+        out._backward = _backward
+        return out
+
+    def __mul__(self, other):
+        if not isinstance(other, Tensor):
+            other = Tensor(other)
+
+        out = Tensor(self.value * other.value)
+        out._parents = [self, other]
+
+        def _backward():
+            self.grad += other.value * out.grad
+            other.grad += self.value * out.grad
+
+        out._backward = _backward
+        return out
+
+    def __radd__(self, other):
+        return self + other
+
+    def __rmul__(self, other):
+        return self * other
+
+    def __sub__(self, other):
+        if not isinstance(other, Tensor):
+            other = Tensor(other)
+        return self + (-other)
+
+    def __neg__(self):
+        out = Tensor(-self.value)
+        out._parents = [self]
+
+        def _backward():
+            self.grad -= out.grad
+
+        out._backward = _backward
+        return out
